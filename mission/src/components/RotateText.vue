9c9f60000d471531558e3aab4f3c48df1c01cb51
jooyoung cho <jzo09@naver.com>
2022-01-05 21:52:01 +0900

    ✨ feat: check 버튼추가(클릭갯수, 현재 문자열 alert처리 및 최대글자 제한)

--- mission/src/components/RotateText.vue
+++ mission/src/components/RotateText.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-        <input v-model="inputText" type="text"/>
+        <input v-model="inputText" type="text" maxlength="20"/>
         <button @click="rotate">text 회전</button>
+        <button @click="checkCurrentyTextCount">Check</button>
+        <div v-if="this.inputText.length>=20">최대 20자 까지 입력 가능합니다.</div>
         <div> {{inputText}}</div>
     </div>
 </template>
@@ -16,12 +18,17 @@ export default {
   data() {
     return {
       inputText: this.defaultText,
+      count: 0,
     }
   },
 
   methods: {
     rotate() {
       this.inputText = this.inputText.slice(1) + this.inputText.slice(0, 1)
+      this.count += 1
+    },
+    checkCurrentyTextCount() {
+      alert(`입력된 문자열:${this.inputText} 지금까지 클릭갯수:${this.count}`)
     },
   },
 }
