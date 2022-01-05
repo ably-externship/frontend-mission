df84f20d8286f07be036bcc57d2008b5968d3563
jooyoung cho <jzo09@naver.com>
2022-01-05 18:44:07 +0900

    ✨ feat:  rotateText 컴포넌트 구현

---
+++ mission/src/components/RotateText.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input v-model="inputText" type="text">
+        <button @click="rotate">text 회전</button>
+        <div> {{inputText}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    defaultText: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      inputText: this.defaultText,
+    };
+  },
+
+  methods: {
+    rotate() {
+      this.inputText = this.inputText.slice(1) + this.inputText.slice(0, 1);
+    },
+  },
+};
+</script>
