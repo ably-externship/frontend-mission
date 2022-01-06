fada2de10ff58769d7fa5af70f09610892bbf216
NaDongHyeon <skehdgus401@gmail.com>
2022-01-06 17:21:47 +0900

    test

--- mission/src/components/HelloWorld.vue
+++ mission/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>project lion</h2>
+    
   </div>
 </template>
 
@@ -13,7 +15,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped"  attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
