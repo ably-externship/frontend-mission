d2018958f3ee9881bbb72cf87fa1de96841e9ee1
Seryeong <99seryoung50@gmail.com>
2022-01-09 21:22:51 +0900

    feat/제출 button

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+    <input v-model="message">
+    <button v-on:click="flip">제출</button>
+    <button v-on:click="warn('Form cannot be submitted yet.', $event)">알림</button>
+    <p>{{ message }}</p>
+    </div>
   </div>
 </template>
 
@@ -13,6 +19,17 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      message: '',
+      count: 1,
+    };
+  },
+  methods: {
+    flip() {
+      this.message = this.message.substring(1) + this.message.substring(0, 1);
+    },
   },
 };
 </script>
