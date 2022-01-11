bc115f3891575311034b4276ddaada65a4aa7b83
jooyoung cho <jzo09@naver.com>
2022-01-11 20:32:35 +0900

    🐛 fix: lint 수정

--- mission/src/components/RotateText.vue
+++ mission/src/components/RotateText.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input v-model="inputText" type="text" maxlength="20"/>
+        <input v-model="inputText" type="text" data-test="text-input-customInput" maxlength="20"/>
         <button @click="rotate">text 회전</button>
         <button @click="checkCurrentyTextCount">Check</button>
         <div v-if="this.inputText.length>=20">최대 20자 까지 입력 가능합니다.</div>
@@ -28,6 +28,7 @@ export default {
       this.count += 1
     },
     checkCurrentyTextCount() {
+      // eslint-disable-next-line no-alert
       alert(`입력된 문자열:${this.inputText} 지금까지 클릭갯수:${this.count}`)
     },
   },
