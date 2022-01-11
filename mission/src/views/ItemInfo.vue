03e08beaa5eb737919d2ddd6874a59838e32aefe
Lawrence Kay <me@lkaybob.pe.kr>
2022-01-11 23:28:48 +0900

    [Week 2] Add template & mission docs * Add template code for week 2 * Add Mission.md for week 2 * Add ESLint checker on PR

---
+++ mission/src/views/ItemInfo.vue
@@ -0,0 +1,24 @@
+<template>
+<div id='item-info-page'></div>
+</template>
+
+<script>
+export default {
+  name: 'ItemInfoPage',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
