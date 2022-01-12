849883dd467af82bd61e5a0ca52deabf57c75666
jooyoung cho <jzo09@naver.com>
2022-01-12 15:40:41 +0900

    ♻️ refactor: 테스트코드 추가

--- mission/tests/unit/rotateTest.spec.js
+++ mission/tests/unit/rotateTest.spec.js
@@ -1,8 +1,16 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import RotateText from '@/components/RotateText.vue'
 
 describe('RotateText.vue', () => {
-  it('RotateText test', () => {
+  it('renders text type input', () => {
+    const testText = 'test'
+    const wrapper = shallowMount(RotateText, {
+      props: { testText },
+    })
+
+    expect(wrapper.find('[data-test="text-input-customInput"]').exists()).toBe(true)
+  })
+  it('RotateText logic test', () => {
     const defaultText = '123456'
     const wrapper = shallowMount(RotateText, {
       props: { defaultText },
