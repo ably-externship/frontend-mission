7477dc0d5b13d9501d067daa72a8bb62955ab669
NaDongHyeon <skehdgus401@gmail.com>
2022-01-16 21:08:18 +0900

    week2 mission

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <router-view />
+  <footer>
+    <div id="rectangle">
+      200,000원 구매
+    </div>
+  </footer>
 </template>
 
 <style>
@@ -9,5 +14,25 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+html, body {
+  margin: 0;
+}
+footer {
+  position: relative;
+  background-color: gray; /*임의색상*/
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 6rem;
+  text-align: center;
+}
+#rectangle {
+  width: 150px;
+  height: 40px;
+  background: black;
+  color:white;
+  display: inline-block;
+  margin-top: 20px
 }
 </style>
--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,13 +1,57 @@
 <template>
-<div id='item-info-page'></div>
+<div id='item-info-page'>
+  <img class='main-image' src='../img/nike.jpeg'>
+  <div class="product-name>">
+    <p class="profile-box" style="background: #BDBDBD;">
+        <img class="profile-img" src="../img/profile.png">
+    </p>
+    <p class="product-name">
+      {{ productName }}
+    </p>
+    <p class="product-tag">
+      {{ productTag }}
+    </p>
+    <p class='star-icon'>
+      <img src='../img/star.svg'>
+    </p>
+    <hr>
+  </div>
+  <div class="product-info">
+    <h2> 나이키 콜라보 제품 </h2>
+    <p class='product-sale'>
+      {{ productSale }}
+    </p>
+    <p class='sale-price'>
+      {{ salePrice }}
+    </p>
+    <p class='origin-price'>
+      {{ originPrice }}
+    </p>
+  </div>
+  <div class='product-detail' style='margin-top: 100px'>
+    <h3 style='margin-left: -300px; font-size: 20px; color: gray'> 상품 정보 </h3>
+    <h3 style='margin-left:-250px; font-size: 10px'> {{ productTitle }} </h3>
+    <img class="wear-img" src="../img/nike_shoes_wear.jpeg">
+    <p class='product-content'>
+      {{ productContent }}
+    </p>
+  </div>
+</div>
 </template>
 
 <script>
+
 export default {
   name: 'ItemInfoPage',
   data() {
     return {
-
+      productName: 'AirMax 405',
+      productTag: '#나이키  #신발',
+      productSale: '34%',
+      salePrice: '200,000원',
+      originPrice: '500,000원',
+      productTitle: '남녀 모두 잘 어울리는 스피드 슈즈',
+      productContent: '당신의 러닝을 책임질수 있는 슈즈 판매합니다.',
     };
   },
   methods: {
@@ -20,5 +64,59 @@ export default {
 </script>
 
 <style scoped>
-
+.main-image, .wear-img {
+  width: 374px;
+  height: 374px;
+}
+.profile-box {
+  width: 50px;
+  height: 50px;
+  border-radius: 70%;
+  overflow: hidden;
+  margin-left: 20px;
+}
+.profile-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.product-name {
+  margin-top: -60px;
+  margin-left: -160px;
+  font-weight: bold;
+  font-size: 12px;
+}
+.product-tag {
+  margin-top: -10px;
+  margin-left: -160px;
+  font-weight: bold;
+  font-size: 10px;
+}
+.star-icon {
+  margin-top: -30px;
+  margin-right: -340px;
+}
+.product-info{
+  float: left;
+  margin-left: 10px
+}
+.product-sale{
+  margin-top: -10px;
+  margin-left: -130px;
+  color: red;
+}
+.sale-price{
+  margin-top: -38px;
+  margin-left: -10px;
+}
+.origin-price{
+  margin-top: -34px;
+  margin-right: -120px;
+  font-size: 10px;
+  text-decoration: line-through;
+  color: gray;
+}
+.product-content{
+  margin-left: -100px;
+}
 </style>
