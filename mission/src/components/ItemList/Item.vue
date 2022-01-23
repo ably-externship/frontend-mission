527e483bf7ce76565554f37190604db4950908d9
NaDongHyeon <skehdgus401@gmail.com>
2022-01-23 18:54:21 +0900

    delete data 2

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="item-list-item">
     <div id="item-container">
-      <img :src="itemlist[0].image"/>
-      <span id="sale price"> {{itemlist[0].price}}</span>
     </div>
   </div>
 </template>
@@ -21,56 +19,56 @@ export default {
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-          {
+        },
+        {
           id: 1,
           name: '신발러닝 업그레이드 나이키',
           image: 'https://images.footlocker.com/is/image/FLEU/315550797302_01?wid=763&hei=538&fmt=png-alpha',
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-          {
+        },
+        {
           id: 2,
           name: '신발러닝 업그레이드 나이키',
           image: 'https://images.footlocker.com/is/image/FLEU/315550797302_01?wid=763&hei=538&fmt=png-alpha',
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-          {
+        },
+        {
           id: 3,
           name: '신발러닝 업그레이드 나이키',
           image: 'https://images.footlocker.com/is/image/FLEU/315550797302_01?wid=763&hei=538&fmt=png-alpha',
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-          {
+        },
+        {
           id: 4,
           name: '신발러닝 업그레이드 나이키',
           image: 'https://images.footlocker.com/is/image/FLEU/315550797302_01?wid=763&hei=538&fmt=png-alpha',
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-          {
+        },
+        {
           id: 5,
           name: '신발러닝 업그레이드 나이키',
           image: 'https://images.footlocker.com/is/image/FLEU/315550797302_01?wid=763&hei=538&fmt=png-alpha',
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-          {
+        },
+        {
           id: 6,
           name: '신발러닝 업그레이드 나이키',
           image: 'https://images.footlocker.com/is/image/FLEU/315550797302_01?wid=763&hei=538&fmt=png-alpha',
           price: 102020,
           original_price: 202020,
           description: 'ㅇㅇㅇㅇ',
-          },
-      ]
+        },
+      ],
     };
   },
 };
