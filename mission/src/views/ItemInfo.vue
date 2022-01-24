a35ef02f8e8e0f651c20511bbb83313d008139cf
NaDongHyeon <skehdgus401@gmail.com>
2022-01-24 09:43:31 +0900

    week 2 mission answer

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,122 +1,257 @@
 <template>
-  <div id='item-info-page'>
-    <div class="container" data-test="product-name">
-      {{ name }}
+  <div id="item-info-page">
+    <div class="image-container">
+      <img class="image-placeholder" :src="image" />
     </div>
-    <div class="container" data-test="product-name">
-      {{ name }}
+    <div class="w3-container seller-panel">
+      <img class="w3-circle seller-image" :src="seller.profile_image" />
+      <div class="seller-info">
+        <div>
+          <strong>{{ seller.name }}</strong>
+        </div>
+        <div class="tag-container">
+          <div class="w3-left" v-for="tag in seller.hash_tags" :key="tag">
+            #{{ tag }}
+          </div>
+        </div>
+      </div>
+      <span style="font-size: 30px; padding-top: 10px; padding-bottom: 10px">
+        <i class="far fa-star w3-right"></i>
+      </span>
     </div>
-    <div class="container" data-test="product-name">
-      <div data-test="product-price">{{ displayPrice }}</div>
-      <div data-test="original-price" v-if="isDiscounted">{{ displayOriginalPrice }}</div>
-    </div>
-     <!-- v-html이란? html를 랜더링하기위한 것 따로 있음. data에서 html 가져오기 때문이지. -->
-    <div class="container">
-      <html v-html="description" data-test="product-description">
-      </html>
-    </div>
-    <div class="container">
-      <div v-bind= "review in reviews" :keys="review.title">
-        <div data-test="review-description">
-          {{review.description}}
+    <div class="w3-container">
+      <h2 data-test="name">{{ name }}</h2>
+      <div class="price-container">
+        <div
+          v-if="isDiscounted()"
+          data-test="discount-rate"
+          class="w3-left price"
+          id="discount"
+        >
+          {{ display_discount_rate }}
+        </div>
+        <div class="w3-left price" data-test="price">
+          {{ priceStringWithComma(isDiscounted() ? price : original_price) }}
+        </div>
+        <div v-if="isDiscounted()" class="w3-left" id="original">
+          <del>{{ priceStringWithComma(original_price) }}</del>
         </div>
       </div>
     </div>
-    <div id="reviews">
-      <div
+    <div class="w3-panel">
+      <div class="w3-row">
+        <h4>상품정보</h4>
+      </div>
+      <div class="w3-row">
+        <html v-html="description"></html>
+      </div>
+      <div class="w3-row">
+        <h4>리뷰</h4>
+      </div>
+      <div id="reviews">
+        <div
           class="review w3-row"
           v-for="review in reviews"
           :key="review.review_no"
-          >
+        >
           <div class="review-text">
-              <div class="review-row-1">
-                  <div class="review-writer" data-test="review-writer">
-                      {{ review.writer }}
-                  </div>
-                  <div class="review-created" data-test="review-created">
-                      {{ review.created }}
-                  </div>
+            <div class="review-row-1">
+              <div class="review-writer" data-test="review-writer">
+                {{ review.writer }}
               </div>
-              <div class="review-title" data-test="review-title">
-                  {{ review.title }}
+              <div class="review-created" data-test="review-created">
+                {{ review.created }}
               </div>
-              <div class="review-content" data-test="review-content">
-                  {{ review.content }}
-              </div>
+            </div>
+            <div class="review-title" data-test="review-title">
+              {{ review.title }}
+            </div>
+            <div class="review-content" data-test="review-content">
+              {{ review.content }}
+            </div>
           </div>
           <div
-              v-if="doesReviewImgExists(review)"
-              class="review-img"
-              data-test="review-img"
-              >
-              <img class="w3-right" :src="review.img" />
+            v-if="doesReviewImgExists(review)"
+            class="review-img"
+            data-test="review-img"
+          >
+            <img class="w3-right" :src="review.img" />
+          </div>
         </div>
+      </div>
+      <div id="footer">
+        <button
+          id="btn-pruchase"
+          class="w3-round-large"
+          data-test="footer-price"
+        >
+          {{ `${priceStringWithComma(price)} 구매` }}
+        </button>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'ItemInfoPage',
   data() {
     return {
       product_no: 1,
-      name: '신발러닝 업그레이드 나이키',
-      image: 'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.tmon.co.kr%2Fdeal%2F4636762258&psig=AOvVaw1cQkulhKK9CmZnZmU3igG1&ust=1642997733582000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjwltSBx_UCFQAAAAAdAAAAABAF',
+      name: '핏이 좋은 수트',
+      image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
       price: 198000,
-      original_price: 300000,
-      description: '<div><p><strong>남녀 모두 잘 어울리는 스피드 슈즈<strong><p><img style="width: 100%" src="http://img3.tmon.kr/cdn3/deals/2020/11/09/4636762258/4636762258_intro_je7JgHzEzp.jpg"/><p>연말 송년회에 아주 어울릴 신발 판매 합니다.</p></div>',
+      original_price: 298000,
+      description: `<div><p><strong>체형에 관계없이 누구에게나 맞는 수트!</strong></p>
+      <img style="width: 100%;" src="https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-2.png"/>
+      <p>연말 송년회에 아주 어울릴 수트 판매합니다!</p></div>`,
       seller: {
         seller_no: 1,
-        name: '대한 슈즈',
-        hash_tags: ['남성', '신발'],
-        profile_image: 'http://image.auction.co.kr/itemimage/1c/63/7c/1c637c5096.jpg',
+        name: '대한양복',
+        hash_tags: ['남성', '수트'],
+        profile_image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/sellers/seller.png',
       },
       reviews: [
         {
           review_no: 1,
-          writer: 'ddce',
-          title: '굿굿',
-          content: '신발이 아주 편해요 만족!',
-          like_count: 21,
-          created: '2021.12.03',
-          img: 'http://img3.tmon.kr/cdn3/deals/2020/11/09/4636762258/4636762258_intro_je7JgHzEzp.jpg',
+          writer: 'lk***',
+          title: '만족해요',
+          content: '핏이 아주 잘 맞습니다. 대만족!',
+          likes_count: 7,
+          created: '2021. 12. 04',
+          img: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/example.jpg',
         },
         {
-          review_no: 2,
-          writer: 'wkcc',
-          title: '좋아요',
-          content: '남편이 좋아해요! ',
-          like_count: 2,
-          created: '2021.01.23',
-          img: 'http://img3.tmon.kr/cdn3/deals/2020/11/09/4636762258/4636762258_intro_je7JgHzEzp.jpg',
+          review_no: 1,
+          writer: 'll***',
+          title: '만족해요2',
+          content: '배송도 빠르고 만족합니다.',
+          likes_count: 5,
+          created: '2021. 12. 05',
         },
       ],
     };
   },
   methods: {
-
+    isDiscounted() {
+      return Object.prototype.hasOwnProperty.call(this.$data, 'original_price');
+    },
+    priceStringWithComma(value) {
+      return `${value.toLocaleString()}원`;
+    },
+    doesReviewImgExists(review) {
+      return Object.prototype.hasOwnProperty.call(review, 'img');
+    },
   },
   computed: {
-    displayPrice() {
-      // 1000 단위로 콤마 표시해주는 함수이다. 이런것을 computed에다가 만들어야 한다.
-      return `${this.price.toLocaleString()}원`;
-    },
-    displayOriginalPrice() {
-      return Object.prototype.hasOwnProperty.call(this.$date, 'original_price')
-        ? `${this.original_price.toLocaleString()}원`
-        : undefined;
-    },
-    isDiscounted() {
-      // 데이터가 object이기 때문에 object에 키가 있다고 하는것을 보기위해서 사용한다.
-      // but eslint가 걸린다.
-      // this.$data.hasOwnProperty('original_price');
-      return Object.prototype.hasOwnProperty.call(this.$data, 'original_price');
+    display_discount_rate() {
+      const rate = ((this.original_price - this.price) / this.original_price) * 100;
+      return `${Number.prototype.toFixed.call(rate, 0)}%`;
     },
   },
 };
 </script>
 
 <style scoped>
-
+.image-container {
+  width: 100%;
+  padding-top: 100%;
+  background-color: gray;
+}
+.image-placeholder {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+.seller-panel {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  display: flex;
+  align-items: center;
+  border-top: 1px solid gainsboro;
+  border-bottom: 1px solid gainsboro;
+}
+.seller-image {
+  height: 50px;
+  width: 50px;
+  border: 1px solid black;
+  margin-right: 10px;
+}
+.seller-info {
+  margin-right: auto;
+}
+.price-container {
+  display: flex;
+  align-items: center;
+}
+.price {
+  padding-right: 10px;
+  font-size: 22px;
+}
+#discount {
+  color: red;
+}
+#original {
+  color: gray;
+}
+.tag-container > div {
+  margin-right: 10px;
+}
+.review {
+  display: flex;
+  border-bottom: 2px solid lightslategray;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+}
+.review-row-1 {
+  display: flex;
+}
+.review-text {
+  margin-right: auto;
+}
+.review-img > img {
+  height: 100px;
+}
+.review-writer {
+  font-size: large;
+  font-weight: bold;
+  color: rgb(116, 116, 116);
+}
+.review-created {
+  font-size: small;
+  margin-left: 10px;
+  color: rgb(116, 116, 116);
+}
+.review-title {
+  font-size: large;
+  font-weight: bold;
+}
+#footer {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  padding: 0;
+  z-index: 100;
+  text-align: center;
+  background-color: white;
+  border-top: 1px solid lightslategray;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#btn-pruchase {
+  background-color: black;
+  color: white;
+  font-size: x-large;
+  padding: 5px 15px;
+  box-shadow: 2px 2px 2px gray;
+}
+#item-info-page {
+  padding-bottom: 60px;
+}
 </style>
--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -2,43 +2,15 @@ import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemInfo.vue';
 
 describe('ItemInfoPage', () => {
-  it('renders ItemInfoPage', () => {
+  it('redners ItemInfoPage', () => {
     const wrapper = mount(ItemInfoPage);
 
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
-    expect(wrapper.find('[data-test="product-image"]').exists()).toBe(true);
   });
 
-  it('renders seller info', () => {
-    const testSellerName = 'test seller';
-    const testSellerImg = 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png';
-    const testSellerTags = ['testing', 'tags'];
-    const resultTagText = '#testing #tags';
-
+  it('renders information on item', () => {
+    const testProductName = 'Testing with vue-test-utils';
     const wrapper = mount(ItemInfoPage, {
-      data() {
-        return {
-          seller: {
-            name: testSellerName,
-            profile_image: testSellerImg,
-            hash_tags: testSellerTags,
-          },
-        };
-      },
-    });
-
-    expect(wrapper.find('[data-test="seller-name"]').text()).toBe(testSellerName);
-    expect(wrapper.find('img[data-test="seller-image"]').attributes('src')).toBe(testSellerImg);
-    expect(wrapper.find('[data-test="seller-hashtags"]').text()).toBe(resultTagText);
-  });
-
-  it('renders product name on item', () => {
-    const testProductName = 'test product name';
-    const wrapper = mount(ItemInfoPage, {
-    // await -> setData는 컴포넌트에서 정의하는 데이터 부분에서 이 데이터를 업데이트 해줌
-    // setData를 하는 메소드가 기존에 정의되어 있는 데이터 프로퍼트와 merge한다.
-    // await wrapper.setData({name: 'test product name'})
-      // merge 가 되는 상황에서 마운트 된다.
       data() {
         return {
           name: testProductName,
@@ -46,76 +18,53 @@ describe('ItemInfoPage', () => {
       },
     });
 
-    expect(wrapper.get('[data-test="product-name"]').text()).toBe(testProductName);
+    expect(wrapper.get('[data-test="name"]').text()).toBe(testProductName);
   });
 
-  it('renders product description on item', () => {
-    const testDescription = '<p>test product description</p>';
-    const wrapper = mount(ItemInfoPage, {
-      data() {
-        return {
-          description: testDescription,
-        };
-      },
-    });
-    expect(wrapper.get('[data-test="product-description"]').html()).toContain(testDescription);
-  });
+  it('render original price if product is discounted', () => {
+    const testOriginalPrice = 19800;
+    const testPrice = 12510;
+    const testDistcountRate = ((testOriginalPrice - testPrice) / testOriginalPrice) * 100;
 
-  it('renders price on item', () => {
-    const testPrice = 100000;
-    const displayTestPrice = '10,000원';
     const wrapper = mount(ItemInfoPage, {
       data() {
-        return {
-          price: testPrice,
-        };
-      },
-    });
-    expect(wrapper.get('[data-test="product-price"]').text()).toBe(displayTestPrice);
-    expect(wrapper.find('[data-test="original-price"]').exists()).toBe(false);
-  });
-
-  it('renders discounted price on item', () => {
-    const testOriginalPrice = 100000;
-    const displayOriginalPrice = '10,000원';
-    const testDiscountedPrice = 5000;
-    const displayDiscountedPrice = '10,000원';
-    const wrapper = mount(ItemInfoPage, {
-      data() {
-        return {
-          price: testDiscountedPrice,
-          original_price: testOriginalPrice,
-        };
+        return { original_price: testOriginalPrice, price: testPrice };
       },
     });
 
-    expect(wrapper.get('[data-test="product-price"]').text()).toBe(displayDiscountedPrice);
-    expect(wrapper.get('[data-test="original-price"]').text()).toBe(displayOriginalPrice);
+    expect(wrapper.find('#original').exists()).toBe(true);
+    expect(wrapper.get('#original').text()).toBe(`${testOriginalPrice.toLocaleString()}원`);
+    expect(wrapper.get('[data-test="discount-rate"]').text()).toBe(`${testDistcountRate.toFixed(0)}%`);
+    expect(wrapper.get('[data-test="price"]').text()).toBe(`${testPrice.toLocaleString()}원`);
   });
 
-  it('renders review', () => {
-    const testReview = [
-      {
-        review_no: 1,
-        writer: 'abc**',
-        title: 'test title',
-        description: 'test description',
-        created: '2021.01.13',
-      },
-    ];
+  it('renders reviews', () => {
+    const testReveiwNo = 3;
+    const testReviewWriter = 'test';
+    const testReviewTitle = 'test review title';
+    const testReviewContent = 'test review content';
+    const testReviewCreated = '2020. 12. 30';
 
     const wrapper = mount(ItemInfoPage, {
       data() {
         return {
-          reviews: testReview,
+          reviews: [{
+            review_no: testReveiwNo,
+            writer: testReviewWriter,
+            title: testReviewTitle,
+            content: testReviewContent,
+            created: testReviewCreated,
+          }],
         };
       },
     });
-    for (let i = 0; i < testReview.length; i += 1) {
-      const review = testReview[i];
-      expect(wrapper.get('[data-test="review-title"]'.text()).toBe());
-      expect(wrapper.get('[data-test="review-description"]').text()).toBe(review.description);
-    }
+
+    expect(wrapper.find('#reviews').exists()).toBe(true);
+    expect(wrapper.find('[data-test="review-writer"]').text()).toBe(testReviewWriter);
+    expect(wrapper.find('[data-test="review-title"]').text()).toBe(testReviewTitle);
+    expect(wrapper.find('[data-test="review-content"]').text()).toBe(testReviewContent);
+    expect(wrapper.find('[data-test="review-created"]').text()).toBe(testReviewCreated);
+    expect(wrapper.findAll('[data-test="review-img"]').length).toBe(0);
   });
 
   it('renders image on review when exists', () => {
@@ -125,6 +74,11 @@ describe('ItemInfoPage', () => {
       data() {
         return {
           reviews: [{
+            review_no: 1,
+            writer: 'writer',
+            title: 'title',
+            content: 'content',
+            created: '1990.12.01',
             img: testReveiwImgUrl,
           }],
         };
