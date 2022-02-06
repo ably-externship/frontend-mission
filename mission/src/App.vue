0ee44165a250a2c853e08d8fdb2cd12d348c3190
NaDongHyeon <skehdgus401@gmail.com>
2022-02-06 15:46:34 +0900

    fix app.vue

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <router-view />
-
 </template>
+
+<script>
+import 'w3-css/4/w3pro.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-html, body {
-  margin: 0;
 }
 </style>
